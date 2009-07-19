7641bb2c068e59ce39f63556b39357b69459c4ac
nesnidal <nesnidal@gmail.com>
2009-07-19 23:41:01 +0800

    new
    Signed-off-by: Hilmar Lapp <hlapp@gmx.net>

--- gathering_input.pl
+++ gathering_input.pl
@@ -23,9 +23,9 @@ my $blocks = parse(
 
 #need validation of input treefile (missing)
 	#nexml
-	#newick             Newick tree format
-    #nexus               Nexus tree format
-    #nhx                  NHX tree format
+	#newick           Newick tree format
+    #nexus            Nexus tree format
+    #nhx              NHX tree format
     #svggraph         SVG graphical representation of tree
-    #tabtree            ASCII text representation of tree
-    #lintree             lintree output format 
+    #tabtree          ASCII text representation of tree
+    #lintree          lintree output format 
